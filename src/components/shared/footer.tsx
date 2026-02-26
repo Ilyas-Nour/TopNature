a17131d4e3aefa2f61ec6db99cff420e4d90c941
Mr-Ilyas-Nour <187109104+Ilyas-Nour@users.noreply.github.com>
2026-02-26 22:52:52 +0000

    Fix serialization error by making Footer a Client Component

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import React from 'react'
 import Link from 'next/link'
 import { Instagram, Facebook, Twitter, Link as LinkedInIcon, Mail, MapPin, Phone } from 'lucide-react'
 import { AnimatedNavLink } from '../ui/animated-nav-link'
